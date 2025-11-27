28ba6113a8d79e431e4e926e837c56fae0eec892
SimAbild <abildtrup123@gmail.com>
2025-11-27 14:16:45 +0100

    First commit

---
+++ src/main/java/org/example/eksamensprojekte25/EksamensprojektE25Application.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojekte25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensprojektE25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksamensprojektE25Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/eksamensprojekte25/EksamensprojektE25ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojekte25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamensprojektE25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
