8e2a31ae949b0bd6596e0f6796ea318be0c1d7c8
MU-1995 <jynke58@hotmail.com>
2025-12-05 00:22:59 +0100

    mock test af visning af enkelt projekt done

--- src/test/java/org/example/eksamensprojekte25/ProjectControllerTest.java
+++ src/test/java/org/example/eksamensprojekte25/ProjectControllerTest.java
@@ -3,6 +3,7 @@ package org.example.eksamensprojekte25;
 import org.example.eksamensprojekte25.controller.ProjectController;
 import org.example.eksamensprojekte25.model.Employee;
 import org.example.eksamensprojekte25.model.Project;
+import org.example.eksamensprojekte25.model.Task;
 import org.example.eksamensprojekte25.model.Timeslot;
 import org.example.eksamensprojekte25.service.EmployeeService;
 import org.example.eksamensprojekte25.service.ProjectService;
@@ -50,17 +51,13 @@ public class ProjectControllerTest {
 
         //fake employee
         Employee employee = new Employee();
-        employee.setEmployeeID(employeeID);
 
         //fake projekter
         Project managedProject = new Project();
-        managedProject.setProjectID(2);
         Project assignedToProject = new Project();
-        assignedToProject.setProjectID(3);
 
         //fake timeslot
         Timeslot timeslot = new Timeslot();
-        timeslot.setTimeslotID(4);
 
         //simulerer service metode kaldende med vores fake data
         when(employeeService.getEmployeeByID(employeeID)).thenReturn(employee);
@@ -87,8 +84,45 @@ public class ProjectControllerTest {
                 .andExpect(model().attribute("timeslots", List.of(timeslot)));
     }
 
+    //tester showsProject metoden
     @Test
-    void shouldAddProject() throws Exception{
+    void shouldShowProject() throws Exception {
+        //fake projekt id
+        int projectID = 69;
+
+        //fake projekt
+        Project project = new Project();
+
+        //fake task
+        Task task = new Task();
+
+        //fake timeslot
+        Timeslot timeslot = new Timeslot();
+
+        //simulerer service metode kaldende med vores fake data
+        when(projectService.getProjectByID(projectID)).thenReturn(project);
+        when(projectService.getTasksByProjectID(projectID)).thenReturn(List.of(task));
+        when(projectService.getAllTimeslots()).thenReturn(List.of(timeslot));
+
+        //Tester at controller metoden gør hvad den skal, at den returnere html siden,
+        //hvilke model-atributter der eksisterer og at den sender de rigtige værdier over
+        mockMvc.perform(get("/project/{projectID}", projectID))
+                .andExpect(status().isOk())
+                .andExpect(view().name("showsProject"))
+                //eksisterende attributter(nøglerne), som vi sender over til html siden fra controller metoden
+                .andExpect(model().attributeExists(
+                        "project",
+                        "tasks",
+                        "timeslots"
+                ))
+                //vi giver attributterne vores fake værdier
+                .andExpect(model().attribute("project", project))
+                .andExpect(model().attribute("tasks", List.of(task)))
+                .andExpect(model().attribute("timeslots", List.of(timeslot)));
+    }
+
+    @Test
+    void shouldAddProject() throws Exception {
 
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(1, "Simon", "Sim", "123", true));
@@ -105,15 +139,15 @@ public class ProjectControllerTest {
     }
 
     @Test
-    void shouldSaveProject() throws Exception{
+    void shouldSaveProject() throws Exception {
 
         mockMvc.perform(post("/saveProject")
-                .sessionAttr("employeeID", 1)
-                .param("projectName", "Test Projekt")
-                .param("ProjectDescription", "Beskrivelse")
-                .param("plannedStartDate", "2025-11-01")
-                .param("plannedFinishDate", "2025-12-01")
-                .param("assignedEmployeeIDs", "2", "3"))
+                        .sessionAttr("employeeID", 1)
+                        .param("projectName", "Test Projekt")
+                        .param("ProjectDescription", "Beskrivelse")
+                        .param("plannedStartDate", "2025-11-01")
+                        .param("plannedFinishDate", "2025-12-01")
+                        .param("assignedEmployeeIDs", "2", "3"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/userProjects"));
 
@@ -142,9 +176,9 @@ public class ProjectControllerTest {
     }
 
     @Test
-    void shouldDeleteProject() throws Exception{
+    void shouldDeleteProject() throws Exception {
         mockMvc.perform(post("/deleteProject/{projectID}", 3)
-                .sessionAttr("employeeID", 1))
+                        .sessionAttr("employeeID", 1))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/userProjects"));
 
