8343e55845718bdcc0829ae515eb5f43c578d82d
SimAbild <abildtrup123@gmail.com>
2025-12-15 10:12:46 +0100

    ændret testprofil måske

--- src/test/java/org/example/eksamensprojekte25/EksamensprojektE25ApplicationTests.java
+++ src/test/java/org/example/eksamensprojekte25/EksamensprojektE25ApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.eksamensprojekte25;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EksamensprojektE25ApplicationTests {
 
 	@Test
